003e2d81f0dc8c591e1c191eeef7367e3965bc96
arteemvaleev <106544155+arteemvaleev@users.noreply.github.com>
2022-11-09 17:15:57 +0400

    Add files via upload

---
+++ files/apartment.js
@@ -0,0 +1,22 @@
+$(function () {
+
+   var mixer = mixitup('.popup-apartmemnt__inner', {
+      load: {
+         filter: '.description'
+      }
+   });
+})
+$(function () {
+   $("#slider-range").slider({
+      range: true,
+      min: 0,
+      max: 500000,
+      values: [100000, 400000],
+      slide: function (event, ui) {
+         $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+      }
+   });
+
+   $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+      " - $" + $("#slider-range").slider("values", 1));
+});
